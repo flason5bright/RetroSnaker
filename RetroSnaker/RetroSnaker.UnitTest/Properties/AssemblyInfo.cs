19ea7b2bca5e5e98537e3cfdec2622db8e419145
fengyong <Yongfeng.Feng@unisys.com>
2019-05-24 09:49:53 +0800

    Add Unit Test

---
+++ RetroSnaker/RetroSnaker.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RetroSnaker.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RetroSnaker.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3f57965-b82d-4780-8c9e-76117a899455")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RetroSnaker/RetroSnaker.UnitTest/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RetroSnaker.UnitTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			string a = "hello";
+			var b = a;
+			Assert.AreEqual(a, b);
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			int a = 1;
+			var b = a;
+			var c = 1;
+			var d = 1;
+			var e = 2;
+			Assert.AreEqual(a, b);
+			Assert.AreEqual(a, c);
+			Assert.AreEqual(d, e);
+			//throw new NullReferenceException();
+		}
+	}
+}
